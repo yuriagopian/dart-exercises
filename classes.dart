f8b024114a7c5aa296b5ec49dadf5337d2a1ced1
yuri agopian <yuri.agopian@hotmail.com>
2021-10-29 09:21:31 -0300

    wip: Expanning class

--- classes.dart
+++ classes.dart
@@ -1,3 +1,43 @@
+/**
+ * Fala pessoal!
+
+Junto com a nova versão do Flutter, foi lançada uma nova funcionalidade chamada NullSafety, que nos obriga a fazer uma verificação sobre um valor para evitar que ele seja nulo e cause problemas no código e no app em alguma situação.
+
+Durante os próximos projetos, vocês podem deparar-se algumas vezes com a mensagem: "The parameter 'valor' can't have a value of 'null'"; quando você está definindo uma nova variável sem atribuir nenhum valor a ela,  você receberá a mensagem "Non-nullable instance field must be initialized".
+
+Por exemplo, caso você faça o seguinte:
+
+String texto;
+List lista;
+int numero;
+Você  se deparará com uma das mensagens de erro. Isso é simples de corrigir, e você tem duas opções: é possível simplesmente atribuir algum valor inicial, mesmo que seja algum valor vazio. Dessa forma, o problema com o NullSafety nunca irá ocorrer. Ou seja, para corrigir bastaria fazer dessa forma:
+
+String texto = '';
+List lista = [];
+int numero = 0;
+Porém, nem sempre é  viável atribuir algum valor padrão para uma de nossas variáveis. Nesse caso, o que precisamos fazer é marcar a variável como "nullable" utilizando a interrogação:
+
+String? texto;
+List? lista;
+int? numero;
+Quando marcado com a "?", estamos sinalizando para o dart que aquela variável é "nullable", e ele deixará que você crie a variável sem um valor padrão. Porém, ao tentar utilizar essas variáveis, você poderá deparar-se com o erro "The argument type can't be assigned to the parameter type". Isso acontece porque, ao utilizar-se da interrogação, é necessário que você também marque o uso dessa variável marcada. Ou seja, quando você for utilizar as variáveis marcadas você precisa fazer uma de duas coisas:
+
+Ou você marca a variável com uma exclamação "!", informando ao Dart que estamos garantindo que aquela variável não será nula:
+
+texto!
+lista!
+numero!
+
+Ou você faz utilização do casting, que se trata de explicitar para o dart qual é a tipagem daquela variável, dessa forma:
+
+texto as String
+lista as List
+numero as int
+Basicamente, é assim que lidamos com a questão do NullSafety. No decorrer do curso, você se deparará diversas vezes com algum desses erros, que são bem simples de corrigir.
+
+Bons estudos!
+ */
+
 class Produto {
   String nome = '';
   double preco = 0;
@@ -31,7 +71,9 @@ imprimirProduto({String nome = '', double preco = 0}) {
   print('O produto ${nome} tem preco R\$${preco}!!!');
 }
 
-imprimirProduto2(int qtde, {String nome = '', double preco = 0}) {
+// nullable : String? nome --> ?
+// nullable : String? nome --> ?
+imprimirProduto2(int qtde, {String? nome, double preco = 0}) {
   for (var i = 0; i < qtde; i++) {
     print('O produto ${nome} tem preco R\$${preco}!!!');
   }
